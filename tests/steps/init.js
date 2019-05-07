3106c211ecb886112da9c005fea2a891c83daf0c
Gabor Csuvik <gaborcsuvik@gmail.com>
2019-05-07 12:28:11 +0200

    Add more test cases

--- tests/steps/init.js
+++ tests/steps/init.js
@@ -8,6 +8,8 @@ const init = async () => {
     process.env.restaurants_api   = "https://9vz6omcjfd.execute-api.eu-west-1.amazonaws.com/dev/restaurants"
     process.env.restaurants_table = "restaurants-dev-csuvikg"
     process.env.AWS_REGION        = "eu-west-1"
+    process.env.order_events_stream = 'orders-dev-csuvikg'
+    process.env.restaurant_notification_topic = 'restaurants-dev-csuvikg'
 
     initialized = true
 }
